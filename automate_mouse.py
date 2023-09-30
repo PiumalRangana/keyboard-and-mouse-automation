f3d85abb2414d746c2b1b69f8b0712cf3c1108a7
Piumal Rangana <piumalrangana4@gmail.com>
2023-09-30 09:03:10 +0530

    Added comments.(Documentation)

--- automate_mouse.py
+++ automate_mouse.py
@@ -1,6 +1,6 @@
 import pyautogui
 
-#Open the paint app
+# Open the paint app
 pyautogui.click(74,745)
 pyautogui.countdown(2)
 pyautogui.write("paint")
@@ -8,9 +8,9 @@ pyautogui.countdown(1)
 pyautogui.press("enter")
 pyautogui.countdown(1)
 
-#pyautogui.moveTo(682.5,383.5)
 
 def first_triangle():
+    # First triangle for the square
     distance = 250
     while distance > 0:
         pyautogui.drag(distance,0)
@@ -22,6 +22,7 @@ def first_triangle():
 
 
 def second_triangle():
+    # Second triangle for the square
     distance=250
     while distance > 0:
         pyautogui.drag(-distance,0)
@@ -31,12 +32,14 @@ def second_triangle():
         distance -= 5
 
 def squer():
+    # Combine two triangles to make the square
     first_triangle()
     pyautogui.drag(125,125)
     second_triangle()
 
-pyautogui.moveTo(13,156)
+
 def pattern():
+    # Make a pattern using squares to fill the screen
     for _ in range(2):
         squer()
         pyautogui.drag(125,125)
@@ -44,7 +47,9 @@ def pattern():
         pyautogui.move(125,-375)
     squer()
 
-pattern()
+
+pyautogui.moveTo(13,156) # Move to the starting point on the screen.(Top left corner.)
+pattern() # Calling the function to draw the pattern
 
 #save the painting
 pyautogui.hotkey("alt","f4")
@@ -55,3 +60,4 @@ pyautogui.press("backspace")
 pyautogui.countdown(1)
 pyautogui.write("PyAutoGui_painting")
 pyautogui.press("enter")
+
